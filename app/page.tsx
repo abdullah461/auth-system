c75e0928a2be730be13857a785b370708d1eadd3
abdull461 <abdullahabubakar461@gmail.com>
2024-02-29 23:41:56 +0100

    added a sign-in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { Main } from "next/document"
+import { LoginButton } from "@/components/auth/login-btn"
 
 export default function Home() {
   return (
-   <p className="font-black text-blue-700">Hello world</p>
-  );
+    <main className="flex h-full flex-col items-center justify-center bg-sky-500">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">Auth</h1>
+        <p className="text-white text-lg">A simple authentication system</p>
+        <div>
+          <LoginButton>
+          <Button variant={"secondary"} size="lg">sign in</Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+    )
 }
