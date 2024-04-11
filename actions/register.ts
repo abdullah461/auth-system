b23485667d1bdb686462d58a9f748e33100c9e79
abdull461 <abdullahabubakar461@gmail.com>
2024-04-11 19:35:51 +0100

    passing data to database

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,38 @@
 "use server";
 
+import { db } from "@/lib/db";
 import * as z from "zod"
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 
-export const register = async(values: z.infer<typeof LoginSchema>) => {
-    const validatedFields = LoginSchema.safeParse(values);
+export const register = async(values: z.infer<typeof RegisterSchema>) => {
+    const validatedFields = RegisterSchema.safeParse(values);
     
-    if(!validatedFields){
+    if(!validatedFields.success){
         return{ error: "Invalid fiels!"};
     }
 
-    return{success: "Email sent!"};
+    const {email, password, name } = validatedFields.data;
+    // hash password
+
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        }
+    });
+
+    if(existingUser){
+        return{error: "Email already in use"}
+    }
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password,
+        }
+    });
+
+    // send verification token email
+
+    return{success: "Account Created!"};
 };
