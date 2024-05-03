2a54e1cf97e2decb5644b19f080e9bfe5556e766
abdull461 <abdullahabubakar461@gmail.com>
2024-05-03 08:22:21 +0100

    role configuration

--- auth.ts
+++ auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-// import GitHub from "next-auth/providers/github"
- 
-// export const { handlers, auth } = NextAuth({
-//   providers: [GitHub],
-// })
+
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+import { UserRole } from "@prisma/client";
+
+
 
 export const {
     handlers: { GET, POST},
@@ -16,12 +16,24 @@ export const {
 }  = NextAuth({
     callbacks: {
         async session({token, session}){
+            console.log({sessionToken: token})
             if (token.sub && session.user){
-                session.user.id = token.sub;            }
+                session.user.id = token.sub;
+            }
+            if(token.role && session.user){
+                session.user.role = token.role as UserRole;
+            }
             return session;
         },
 
         async jwt({token}) {
+            if(!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub)
+
+            if(!existingUser) return token;
+
+            token.role = existingUser.role;
             return token;
         }
     },
