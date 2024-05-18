99e1ce427ed01125b7b7fd2b79406168578c65d8
abdull461 <abdullahabubakar461@gmail.com>
2024-05-18 21:17:29 +0100

    creating password reset token

--- actions/reset.ts
+++ actions/reset.ts
@@ -4,6 +4,8 @@ import * as z from "zod";
 
 import { ResetSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { generatePasswordResetToken } from "@/lib/token";
+import { sendPasswordresetEmail } from "@/lib/mail";
 
 export const reset = async (values: z.infer<typeof ResetSchema>) => {
     const validatedFields = ResetSchema.safeParse(values);
@@ -20,7 +22,15 @@ export const reset = async (values: z.infer<typeof ResetSchema>) => {
         return{error: "Email not found!"}
     }
 
-    // generate token and send mail
+    // generate token and send mail 
+    
+    const passwordResetToken = await generatePasswordResetToken(email);
+    await sendPasswordresetEmail(
+        passwordResetToken.email,
+        passwordResetToken.token
+    );
+
+
     return {success: "Reset email sent"}
 
 }
