b4b1a32622c3b9c64dd231790530a11618bcbc7d
abdullah461 <abdullahabubakar461@.com>
2024-07-14 15:09:37 +0100

    corrected typo error from "Get" to "GET"

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -2,7 +2,7 @@ import { currentRole } from "@/lib/auth";
 import { UserRole } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export async function Get() {
+export async function GET() {
   const role = await currentRole();
 
   if (role === UserRole.ADMIN) {
