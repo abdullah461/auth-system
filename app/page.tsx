b3c7c1e03cea22283a2561413d9406f0673fb61d
abdullah461 <abdullahabubakar461@.com>
2024-07-30 12:50:49 +0100

    more ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,30 +8,24 @@ import { LoginButton } from "@/components/auth/login-btn";
 
 export default function Home() {
   return (
-    <main className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-      
-      <div className="flex items-center justify-center py-12">
+    <main className="w-full h-full bg-[#1f75ff] text-white">
+      <div className="h-full flex items-center justify-center">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-xl font-bold">Advanced Auth System</h1>
-            <p>This is an advanced authentication and authorization system built using react, nextjs, Authjs, shadcn-UI, prisma and Neon-DB</p>
+            <h1 className="text-2xl font-bold">Auth System</h1>
+            <p>
+              This is an advanced authentication and authorization system built
+              using react, nextjs, Authjs, shadcn-UI, prisma and Neon-DB
+            </p>
           </div>
           <div className="grid gap-4">
-           
             <LoginButton mode="modal" asChild>
-          <Button variant={"dexlr"} size="lg">Get started</Button>
-          </LoginButton>
+              <Button variant="secondary" size="lg">
+                Get started
+              </Button>
+            </LoginButton>
           </div>
         </div>
-      </div>
-      <div className="hidden bg-muted lg:block" >
-        {/* <Image
-          src=""
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        /> */}
       </div>
     </main>
   );
